2dcec0c633c083ef5f2c9e5546320f5221d610d9
yash3129 <chaurasiayash2910@gmail.com>
2025-07-09 13:12:03 +0530

    Update main.p

--- Backend/main.py
+++ Backend/main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ai-code-humanizer.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
